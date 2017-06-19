e2e0844b539ba4d8855ea7a9856e5662e1502ec1
Carrie Forde <carrie@carrieforde.com>
2017-06-18 19:34:23 -0700

    Adds template part for coming soon content

--- template-coming-soon.php
+++ template-coming-soon.php
@@ -17,7 +17,7 @@ get_header( 'coming-soon' ); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'coming-soon' ); ?>
 
 				<?php
 					// Maybe load comments.
